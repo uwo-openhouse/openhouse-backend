1e696e1817f6aeff0c5329a0e40b9a301a4e872d
Jordan Mathewson <jmathe53@uwo.ca>
2020-01-12 21:33:20 -0500

    Finish CRUD functionality for buildings

--- buildings/index.js
+++ buildings/index.js
@@ -1,20 +1,53 @@
+const aws = require('aws-sdk');
 const status = require('http-status');
+const UUIDv4 = require('uuid/v4');
+const Joi = require('@hapi/joi');
+
+aws.config.update({ region: 'us-east-2' });
+const ddb = new aws.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
+const TABLE_NAME = process.env.TABLE_NAME;
+
+const buildingSchema = Joi.object({
+    name: Joi.string().required(),
+    position: Joi.object({
+        lat: Joi.number().greater(-90).less(90).required(),
+        lng: Joi.number().greater(-180).less(180).required()
+    }).required()
+});
 
 exports.handler = async (event, context) => {
     try {
         switch (event.httpMethod) {
             case 'GET':
                 return getBuildings();
+
             case 'POST':
-                return createBuilding(event.body);
+                return createBuilding(JSON.parse(event.body));
+
             case 'PUT':
-                return updateBuilding(event.body);
+                if (!event.pathParameters || !event.pathParameters.uuid) {
+                    return {
+                        statusCode: status.BAD_REQUEST,
+                        body: JSON.stringify({
+                            error: 'Missing UUID in URL path'
+                        })
+                    }
+                }
+                return updateBuilding(event.pathParameters.uuid, JSON.parse(event.body));
+
             case 'DELETE':
-                return deleteBuilding();
+                if (!event.pathParameters || !event.pathParameters.uuid) {
+                    return {
+                        statusCode: status.BAD_REQUEST,
+                        body: JSON.stringify({
+                            error: 'Missing UUID in URL path'
+                        })
+                    }
+                }
+                return deleteBuilding(event.pathParameters.uuid);
+
             default:
-                return {
-                    statusCode: status.METHOD_NOT_ALLOWED
-                };
+                return { statusCode: status.METHOD_NOT_ALLOWED };
         }
     } catch (err) {
         console.err(err);
@@ -22,32 +55,101 @@ exports.handler = async (event, context) => {
     }
 };
 
-const toBeReplaced = {
-    body: JSON.stringify({
-        success: true
-    }),
-    statusCode: status.OK
-};
-
 async function getBuildings() {
-    // TODO
-    return toBeReplaced;
+    try {
+        const data = await ddb.scan({ TableName: TABLE_NAME }).promise();
+
+        return {
+            statusCode: status.OK,
+            body: JSON.stringify({
+                buildings: data.Items
+            })
+        };
+    } catch (err) {
+        console.error(err);
+        return err;
+    }
 }
 
 async function createBuilding(body) {
-    // TODO
-    return toBeReplaced;
+    try {
+        const { value: building, error } = buildingSchema.validate(body);
+        if (error) {
+            return {
+                statusCode: status.BAD_REQUEST,
+                body: JSON.stringify({
+                    error: error.details.map((error) => error.message).join('; ')
+                })
+            }
+        }
+
+        await ddb.put({
+            TableName: TABLE_NAME,
+            Item: {
+                UUID: UUIDv4(),
+                ...building
+            }
+        }).promise();
+
+        return { statusCode: status.CREATED };
+    } catch (err) {
+        console.error(err);
+        return err;
+    }
 }
 
-async function updateBuilding(body) {
-    // TODO
-    return toBeReplaced;
+async function updateBuilding(uuid, body) {
+    try {
+        // Check to ensure uuid exists first
+        const existingData = await ddb.get({
+            TableName: TABLE_NAME,
+            Key: { UUID: uuid }
+        }).promise();
+        if (!existingData.Item) {
+            return {
+                statusCode: status.NOT_FOUND,
+                body: JSON.stringify({
+                    error: 'Building does not exist'
+                })
+            }
+        }
+
+        const { value: building, error } = buildingSchema.validate(body);
+        if (error) {
+            return {
+                statusCode: status.BAD_REQUEST,
+                body: JSON.stringify({
+                    error: error.details.map((detail) => detail.message).join('; ')
+                })
+            }
+        }
+
+        // Save new building item
+        await ddb.put({
+            TableName: TABLE_NAME,
+            Item: {
+                UUID: uuid,
+                ...building
+            }
+        }).promise();
+
+        return { statusCode: status.OK };
+    } catch (err) {
+        console.error(err);
+        return err;
+    }
 }
 
-async function deleteBuilding(body) {
-    // TODO
-    return toBeReplaced;
+async function deleteBuilding(uuid) {
+    try {
+        await ddb.delete({
+            TableName: TABLE_NAME,
+            Key: { UUID: uuid }
+        }).promise();
+
+        return { statusCode: status.OK };
+    } catch (err) {
+        console.error(err);
+        return err;
+    }
 }
-
-
-
