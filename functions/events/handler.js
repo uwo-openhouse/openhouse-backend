3b3453275cbcc24a66ccb50a97746a2c9ab27f04
Jordan Mathewson <jmathe53@uwo.ca>
2020-02-04 21:38:07 -0500

    Fix error handling in lambdas

--- functions/events/handler.js
+++ functions/events/handler.js
@@ -25,142 +25,124 @@ module.exports = (deps) => async (event) => {
     try {
         switch (event.httpMethod) {
             case 'GET':
-                return getEvents(deps.dynamo);
+                return await getEvents(deps.dynamo);
 
             case 'POST':
-                return createEvent(deps.dynamo, JSON.parse(event.body));
+                return await createEvent(deps.dynamo, JSON.parse(event.body));
 
             case 'PUT':
                 if (!event.pathParameters || !event.pathParameters.uuid) {
                     return response(status.BAD_REQUEST, { error: 'Missing UUID in URL path' });
                 }
-                return updateEvent(deps.dynamo, event.pathParameters.uuid, JSON.parse(event.body));
+                return await updateEvent(deps.dynamo, event.pathParameters.uuid, JSON.parse(event.body));
 
             case 'DELETE':
                 if (!event.pathParameters || !event.pathParameters.uuid) {
                     return response(status.BAD_REQUEST, { error: 'Missing UUID in URL path' });
                 }
-                return deleteEvent(deps.dynamo, event.pathParameters.uuid);
+                return await deleteEvent(deps.dynamo, event.pathParameters.uuid);
 
             default:
                 return response(status.METHOD_NOT_ALLOWED);
         }
     } catch (err) {
         console.error(err);
-        return err;
+        return response(status.INTERNAL_SERVER_ERROR, {
+            error: err.message
+        });
     }
 };
 
 async function getEvents(dynamo) {
-    try {
-        const data = await dynamo.scanEvents();
+    const data = await dynamo.scanEvents();
 
-        for (const event of data.Items) {
-            event.attendees = (await dynamo.getEventAttendees(event.uuid)).Item.attendees;
-        }
-
-        return response(status.OK, data.Items);
-    } catch (err) {
-        console.error(err);
-        return err;
+    for (const event of data.Items) {
+        event.attendees = (await dynamo.getEventAttendees(event.uuid)).Item.attendees;
     }
+
+    return response(status.OK, data.Items);
 }
 
 async function createEvent(dynamo, body) {
-    try {
-        const bodyEvents = [];
+    const bodyEvents = [];
 
-        if (Array.isArray(body)) {
-            bodyEvents.push(...body);
-        } else {
-            bodyEvents.push(body);
-        }
-
-        const validEvents = [];
-        for (let i = 0; i < bodyEvents.length; i++) {
-            const { value: event, error } = eventSchema.validate(bodyEvents[i]);
-            if (error) {
-                return response(status.BAD_REQUEST, {
-                    error: error.details.map((error) => error.message).join('; ') + ` for event with index ${i}`
-                });
-            }
-
-            const verifyError = await verifyUUIDs(dynamo, event.openHouse, event.area, event.building);
-            if (verifyError) {
-                return response(status.BAD_REQUEST, { error: verifyError + ` for event with index ${i}` });
-            }
-
-            validEvents.push(event);
-        }
-
-        const createdEvents = [];
-        for (const event of validEvents) {
-            const uuid = UUIDv4();
-
-            const newEvent = {
-                uuid,
-                ...event
-            };
-            const newAttendees = {
-                uuid,
-                attendees: 0
-            };
-
-            await dynamo.putEvent(newEvent);
-            await dynamo.putEventAttendees(newAttendees);
-
-            createdEvents.push({
-                ...newEvent,
-                ...newAttendees
-            });
-        }
-
-        return response(status.CREATED, Array.isArray(body) ? createdEvents : createdEvents[0]);
-    } catch (err) {
-        console.error(err);
-        return err;
+    if (Array.isArray(body)) {
+        bodyEvents.push(...body);
+    } else {
+        bodyEvents.push(body);
     }
-}
 
-async function updateEvent(dynamo, uuid, body) {
-    try {
-        // Check to ensure uuid exists first
-        const existingData = await dynamo.getEvent(uuid);
-        if (!existingData.Item) {
-            return response(status.NOT_FOUND, { error: 'Event does not exist' });
-        }
-
-        const { value: event, error } = eventSchema.validate(body);
+    const validEvents = [];
+    for (let i = 0; i < bodyEvents.length; i++) {
+        const { value: event, error } = eventSchema.validate(bodyEvents[i]);
         if (error) {
             return response(status.BAD_REQUEST, {
-                error: error.details.map((detail) => detail.message).join('; ')
+                error: error.details.map((error) => error.message).join('; ') + ` for event with index ${i}`
             });
         }
 
         const verifyError = await verifyUUIDs(dynamo, event.openHouse, event.area, event.building);
         if (verifyError) {
-            return response(status.BAD_REQUEST, { error: verifyError });
+            return response(status.BAD_REQUEST, { error: verifyError + ` for event with index ${i}` });
         }
 
-        await dynamo.putEvent({ uuid, ...event });
-
-        return response(status.OK);
-    } catch (err) {
-        console.error(err);
-        return err;
+        validEvents.push(event);
     }
+
+    const createdEvents = [];
+    for (const event of validEvents) {
+        const uuid = UUIDv4();
+
+        const newEvent = {
+            uuid,
+            ...event
+        };
+        const newAttendees = {
+            uuid,
+            attendees: 0
+        };
+
+        await dynamo.putEvent(newEvent);
+        await dynamo.putEventAttendees(newAttendees);
+
+        createdEvents.push({
+            ...newEvent,
+            ...newAttendees
+        });
+    }
+
+    return response(status.CREATED, Array.isArray(body) ? createdEvents : createdEvents[0]);
+}
+
+async function updateEvent(dynamo, uuid, body) {
+    // Check to ensure uuid exists first
+    const existingData = await dynamo.getEvent(uuid);
+    if (!existingData.Item) {
+        return response(status.NOT_FOUND, { error: 'Event does not exist' });
+    }
+
+    const { value: event, error } = eventSchema.validate(body);
+    if (error) {
+        return response(status.BAD_REQUEST, {
+            error: error.details.map((detail) => detail.message).join('; ')
+        });
+    }
+
+    const verifyError = await verifyUUIDs(dynamo, event.openHouse, event.area, event.building);
+    if (verifyError) {
+        return response(status.BAD_REQUEST, { error: verifyError });
+    }
+
+    await dynamo.putEvent({ uuid, ...event });
+
+    return response(status.OK);
 }
 
 async function deleteEvent(dynamo, uuid) {
-    try {
-        await dynamo.deleteEvent(uuid);
-        await dynamo.deleteEventAttendees(uuid);
+    await dynamo.deleteEvent(uuid);
+    await dynamo.deleteEventAttendees(uuid);
 
-        return response(status.OK);
-    } catch (err) {
-        console.error(err);
-        return err;
-    }
+    return response(status.OK);
 }
 
 async function verifyUUIDs(dynamo, openHouseUUID, areasUUID, buildingUUID) {
